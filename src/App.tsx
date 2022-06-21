bcf7f87cf872436665e87fd7a4cd3be055631c14
Vladyslav <vladyslav.hryhoriv@rolique.io>
2022-06-22 00:52:12 +0300

    feat(prettier): Config prettier for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
-  return (
-    <div>
-      Star wars
-    </div>
-  );
+  return <div>Star wars</div>
 }
 
-export default App;
+export default App
